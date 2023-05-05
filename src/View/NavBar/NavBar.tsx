d59c28cc987791b7b624c99f733502673456f76c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-05-06 00:46:21 +0300

    responsive navbar

--- src/View/NavBar/NavBar.tsx
+++ src/View/NavBar/NavBar.tsx
@@ -1,20 +1,24 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useResponsivity } from '../../helper/useWidth'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const NavBar = () => {
     const [toggleMenu, setToggleMenu] = useState(false);
     const isMobile = useResponsivity()
 
+    useEffect(() => {
+        isMobile === false && setToggleMenu(false)
+    }, [isMobile])
+
     return (
         <nav>
-            <button className='navbar__btnMenu' onClick={() => setToggleMenu(!toggleMenu)}>{toggleMenu ? "Close" : "Menu"}</button>
+            <button className='navbar__btnMenu' onClick={() => setToggleMenu(!toggleMenu)}>{toggleMenu ? "X" : "Menu"}</button>
 
             {(toggleMenu || !isMobile) && (
                 <div className='navbar__navigation'>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/more">More</Link>
+                    <NavLink to="/" onClick={() => setToggleMenu(false)}>Home</NavLink>
+                    <NavLink to="/about" onClick={() => setToggleMenu(false)}>About</NavLink>
+                    <NavLink to="/more" onClick={() => setToggleMenu(false)}>More</NavLink>
                 </div>
             )}
         </nav>
